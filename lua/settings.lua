6be9679fcaad8e1b2168a25d9b0ab84d2eb03a23
biubiu <1575071736@qq.com>
2023-04-17 08:57:59 +0800

    first commit

---
+++ lua/settings.lua
@@ -0,0 +1,34 @@
+local global = vim.g
+local o = vim.o
+
+vim.scriptencoding = "utf-8"
+
+-- Map <leader> = the space key
+
+global.mapleader = " "
+global.maplocalleader = " "
+
+-- Editor option
+
+o.number = true
+o.relativenumber = true
+o.clipboard = "unnamedplus"
+o.syntax = "on"
+o.autoindent = true
+o.cursorline = true
+o.expandtab = true
+o.shiftwidth = 2
+o.tabstop = 2
+o.encoding = "utf-8"
+o.ruler = true
+o.mouse = "a"
+o.title = true
+o.hidden = true
+o.ttimeoutlen = 0
+o.wildmenu = true
+o.showcmd = true
+o.showmatch = true
+o.inccommand = "split"
+o.splitbelow = true
+o.splitright = true
+o.termguicolors = true
